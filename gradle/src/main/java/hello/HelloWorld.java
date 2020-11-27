5472ab4236a5353e7664aac6bfca600b27775b7f
Carlos Prado <carlos.prado@portalsolar.com.br>
2020-11-27 01:02:55 -0300

    Add joda-time dep

--- gradle/src/main/java/hello/HelloWorld.java
+++ gradle/src/main/java/hello/HelloWorld.java
@@ -1,9 +1,12 @@
 package hello;
 
-import hello.Greeter;
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+
+        System.out.println("The current local time is: " + currentTime);
         System.out.println(new Greeter().sayHello());
     }
 }
